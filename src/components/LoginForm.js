ecb2afd8e696241d80ef63a2b4da78f929596f74
dlunsford <dlunsford@zulily.com>
2019-06-23 13:33:16 -0700

    adds firebase and config values

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import firebase from 'firebase';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABSE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGE_SENDER_ID,
+  APP_ID,
+} from '../../authSettings';
+
 import { Card, CardSection, Input, Button, Header } from './common';
 import { emailChanged, passwordChanged } from '../actions';
 
 class LoginForm extends Component {
+  componentWillMount() {
+    const firebaseConfig = {
+      apiKey: API_KEY,
+      authDomain: AUTH_DOMAIN,
+      databaseURL: DATABSE_URL,
+      projectId: PROJECT_ID,
+      storageBucket: STORAGE_BUCKET,
+      messagingSenderId: MESSAGE_SENDER_ID,
+      appId: APP_ID,
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
   onEmailChange = text => {
     this.props.emailChanged(text);
   };
