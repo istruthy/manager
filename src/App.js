861910773f8027cde8365dceb737fe9a5c087a7b
dlunsford <dlunsford@zulily.com>
2019-06-23 12:21:30 -0700

    working state for emailChanged

--- src/App.js
+++ src/App.js
@@ -2,27 +2,11 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import firebase from 'firebase';
 import reducers from './reducers';
-require('dotenv').config();
 
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  componentWillMount() {
-    const firebaseConfig = {
-      apiKey: process.env.apiKey,
-      authDomain: process.env.authDomain,
-      databaseURL: process.env.databaseURL,
-      projectId: process.env.projectId,
-      storageBucket: '',
-      messagingSenderId: process.env.messagingSenderId,
-      appId: process.env.appId,
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-  }
-
   render() {
     return (
       <Provider store={createStore(reducers)}>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
 import emailChanged from '../actions';
+import { thisExpression } from '@babel/types';
 
 class LoginForm extends Component {
   render() {
@@ -12,7 +13,12 @@ class LoginForm extends Component {
     return (
       <Card>
         <CardSection>
-          <Input label='email' placeholder='email@gmail.com' onChangeText={this.onEmailChange} />
+          <Input
+            label='email'
+            placeholder='email@gmail.com'
+            onChangeText={this.onEmailChange}
+            value={this.props.email}
+          />
         </CardSection>
         <CardSection>
           <Input label='password' secureTextEntry placeholder='password' />
@@ -25,8 +31,15 @@ class LoginForm extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email,
+  };
+};
+
 export default connect(
-  null,
-  { emailChanged },
-  LoginForm,
-);
+  mapStateToProps,
+  {
+    emailChanged,
+  },
+)(LoginForm);
